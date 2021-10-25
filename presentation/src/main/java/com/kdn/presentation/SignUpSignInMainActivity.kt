cce520ad228763234fdcc206450bcb88bb17b8a4
ParkSangSun <appleasz1028102@gmail.com>
2021-10-25 23:21:39 +0900

    [CREATE] SignUp and SignIn Activity

---
+++ presentation/src/main/java/com/kdn/presentation/SignUpSignInMainActivity.kt
@@ -0,0 +1,12 @@
+package com.kdn.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gsm.presentation.R
+
+class SignUpSignInMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_sign_in_main)
+    }
+}
