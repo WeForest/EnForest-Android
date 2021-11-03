ba925526250a05359341514da6f3356cfd341cdc
KyungMin <2004ckck@naver.com>
2021-11-03 22:24:42 +0900

    init

---
+++ presentation/src/main/java/com/kdn/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.kdn.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gsm.presentation.R
+import com.gsm.presentation.databinding.ActivityMainBinding
+import com.kdn.presentation.base.BaseActivity
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun init() {
+
+    }
+}
