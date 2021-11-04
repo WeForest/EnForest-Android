925a9c9acc47f8f3ae93ac269581b4e7f315256a
ckrudals <2004ckck@naver.com>
2021-11-04 18:04:47 +0900

    [CREATE] Mission entity request,response

---
+++ data/src/main/java/com/gsm/data/entity/mission/response/PatchMissionFailResponse.kt
@@ -0,0 +1,31 @@
+package com.gsm.data.entity.mission.response
+
+
+import com.google.gson.annotations.SerializedName
+
+data class PatchMissionFailResponse(
+    @SerializedName("accessToken")
+    val accessToken: String?,
+    @SerializedName("authCompany")
+    val authCompany: Boolean?,
+    @SerializedName("companyEmail")
+    val companyEmail: String?,
+    @SerializedName("email")
+    val email: String?,
+    @SerializedName("exp")
+    val exp: Int?,
+    @SerializedName("id")
+    val id: Int?,
+    @SerializedName("isJobSeeker")
+    val isJobSeeker: Boolean?,
+    @SerializedName("level")
+    val level: Int?,
+    @SerializedName("name")
+    val name: String?,
+    @SerializedName("profileImg")
+    val profileImg: String?,
+    @SerializedName("purpose")
+    val purpose: String?,
+    @SerializedName("sub")
+    val sub: String?
+)
