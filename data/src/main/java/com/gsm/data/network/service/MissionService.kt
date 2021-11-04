8a29d926df723b41745e79128396c6d5d6265ee6
ckrudals <2004ckck@naver.com>
2021-11-04 18:05:09 +0900

    [CREATE] MissionService 생성

---
+++ data/src/main/java/com/gsm/data/network/service/MissionService.kt
@@ -0,0 +1,41 @@
+package com.gsm.data.network.service
+
+import com.gsm.data.entity.mission.request.AddMissionRequest
+import com.gsm.data.entity.mission.response.*
+import retrofit2.Response
+import retrofit2.http.*
+
+interface MissionService {
+
+    @POST("mission/add")
+    suspend fun addMission(
+        @Body request: AddMissionRequest
+    ): Response<AddMissionResponse>
+
+    @DELETE("mission/{number}")
+    suspend fun deleteMission(
+        @Path("number") number: Int
+    ): Response<DeleteMissioNResponse>
+
+    @GET("mission/{number}")
+    suspend fun getMission(
+        @Path("number") number: Int
+    ): Response<GetMissionResponse>
+
+    @GET("mission/{type}")
+    suspend fun getMissionType(
+        @Path("type") type: String
+    ):Response<GetMissionTypeResponse>
+    @PATCH("/mission/clear/{number}")
+    suspend fun patchMissionClear(
+        @Header("authorization") header : String,
+        @Path("number") number : Int
+    ):Response<PatchMissionClearResponse>
+
+    @PATCH("/mission/fail/{number}")
+    suspend fun patchMissionFail(
+        @Header("authorization") header : String,
+        @Path("number") number : Int
+    ):Response<PatchMissionFailResponse>
+
+}
