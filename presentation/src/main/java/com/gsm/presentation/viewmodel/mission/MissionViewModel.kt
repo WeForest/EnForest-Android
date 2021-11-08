a25d4eee832d5d38082727ebe34672387d8740e4
ckrudals <2004ckck@naver.com>
2021-11-08 20:42:37 +0900

    [ADD] getMissionUseCase

--- presentation/src/main/java/com/gsm/presentation/viewmodel/mission/MissionViewModel.kt
+++ presentation/src/main/java/com/gsm/presentation/viewmodel/mission/MissionViewModel.kt
@@ -5,8 +5,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.gsm.domain.entity.GetMissionEntity
 import com.gsm.domain.entity.request.AddMission
 import com.gsm.domain.usecase.mission.AddMissionUseCase
+import com.gsm.domain.usecase.mission.GetMissionUseCase
+import com.gsm.presentation.util.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -14,7 +17,8 @@ import javax.inject.Inject
 @HiltViewModel
 class MissionViewModel @Inject constructor
     (
-    private val addMissionUseCase: AddMissionUseCase
+    private val addMissionUseCase: AddMissionUseCase,
+    private val getMissionUseCase: GetMissionUseCase
 ) : ViewModel() {
     private val TAG = "mission"
 
@@ -23,10 +27,16 @@ class MissionViewModel @Inject constructor
 
     private val _exp = MutableLiveData<Int>()
     val exp: LiveData<Int> get() = _exp
+    private val _success = MutableLiveData<Event<Boolean>>()
+    private val _missionData = MutableLiveData<GetMissionEntity>()
+    val missionData: LiveData<GetMissionEntity> get() = _missionData
+
+    init {
+        _success.value = Event(false)
+    }
 
 
-    private val _success = MutableLiveData<Boolean>()
-    val success: LiveData<Boolean> get() = _success
+    val success: LiveData<Event<Boolean>> get() = _success
     suspend fun addMission(title: String, content: String, expiredAt: Int, type: String) =
         viewModelScope.launch {
             try {
@@ -44,11 +54,25 @@ class MissionViewModel @Inject constructor
                     )
                 ).let {
                     Log.d("mission", "addMission: 성공 ")
-                    _success.value = true
+                    _success.value = Event(true)
                 }
             } catch (e: Exception) {
                 Log.e(TAG, "addMission: ${e}")
-                _success.value = false
+                _success.value = Event(false)
             }
         }
+
+    suspend fun getMission(number: Int) = viewModelScope.launch {
+
+        try {
+            getMissionUseCase.buildUseCaseObservable(GetMissionUseCase.Params(number)).let {
+                _missionData.value = it
+            }
+
+
+        } catch (e: Exception) {
+
+        }
+
+    }
 }
