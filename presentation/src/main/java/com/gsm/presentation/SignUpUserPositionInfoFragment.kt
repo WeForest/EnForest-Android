e35209b73ba08edbce804be04298f76954abbe00
ParkSangSun <appleasz1028102@gmail.com>
2021-11-08 09:37:40 +0900

    [UPDATE] SignUpUserPositionInfoFragment

---
+++ presentation/src/main/java/com/gsm/presentation/SignUpUserPositionInfoFragment.kt
@@ -0,0 +1,10 @@
+package com.gsm.presentation
+
+import com.gsm.presentation.databinding.FragmentSignUpUserJobSearchBinding
+
+class SignUpUserPositionInfoFragment : BaseFragment<FragmentSignUpUserJobSearchBinding>(R.layout.fragment_sign_up_user_position_info) {
+    override fun init() {
+
+    }
+
+}
