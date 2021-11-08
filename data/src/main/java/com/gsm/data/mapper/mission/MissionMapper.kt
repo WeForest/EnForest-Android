bb847f9bade31c903e8f5f094deabf4376719c16
ckrudals <2004ckck@naver.com>
2021-11-08 20:44:06 +0900

    [ADD] dataBinding 활용할 수 있게 layout 추가

--- data/src/main/java/com/gsm/data/mapper/mission/MissionMapper.kt
+++ data/src/main/java/com/gsm/data/mapper/mission/MissionMapper.kt
@@ -1,8 +1,8 @@
 package com.gsm.data.mapper.mission
 
 import com.gsm.data.entity.mission.request.AddMissionRequest
-import com.gsm.data.entity.mission.response.AddMissionResponse
-import com.gsm.domain.entity.AddMissionEntity
+import com.gsm.data.entity.mission.response.*
+import com.gsm.domain.entity.*
 import com.gsm.domain.entity.request.AddMission
 
 
@@ -19,6 +19,81 @@ fun AddMissionResponse.toDomain(): AddMissionEntity {
     )
 }
 
+fun DeleteMissioNResponse.toDomain(): DeleteMissionEntity {
+    return DeleteMissionEntity(
+        this.content,
+        this.createdAt,
+        this.exp,
+        this.expiredAt,
+        this.id,
+        this.level,
+        this.title,
+        this.type
+    )
+}
+
+fun GetMissionResponse.toDomain(): GetMissionEntity {
+    return GetMissionEntity(
+        this.content,
+        this.createdAt,
+        this.exp,
+        this.expiredAt,
+        this.id,
+        this.level,
+        this.title,
+        this.type
+    )
+}
+
+fun GetMissionTypeResponse.toDomain(): GetMissionTypeEntity {
+    return GetMissionTypeEntity(
+        this.content,
+        this.createdAt,
+        this.exp,
+        this.expiredAt,
+        this.id,
+        this.level,
+        this.title,
+        this.type
+    )
+}
+
+fun PathMissionClearResponse.toDomain(): PathMissionClearEntity {
+    return PathMissionClearEntity(
+
+        this.accessToken,
+        this.authCompany,
+        this.companyEmail,
+        this.email,
+        this.exp,
+        this.id,
+        this.isJobSeeker,
+        this.level,
+        this.name,
+        this.profileImg,
+        this.purpose,
+        this.sub
+    )
+}
+
+
+fun PathMissionFailResponse.toDomain(): PachMissionFailEntity {
+    return PachMissionFailEntity(
+
+        this.accessToken,
+        this.authCompany,
+        this.companyEmail,
+        this.email,
+        this.exp,
+        this.id,
+        this.isJobSeeker,
+        this.level,
+        this.name,
+        this.profileImg,
+        this.purpose,
+        this.sub
+    )
+}
 fun AddMissionEntity.toData(): AddMissionResponse {
     return AddMissionResponse(
         this.content,
@@ -31,7 +106,6 @@ fun AddMissionEntity.toData(): AddMissionResponse {
         this.type
     )
 }
-
 
 
 fun AddMission.toRequest(): AddMissionRequest {
