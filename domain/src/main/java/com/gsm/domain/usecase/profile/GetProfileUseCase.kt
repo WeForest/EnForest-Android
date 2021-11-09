6fe2f3716b2401a27745e3951da99a535f5a7861
choiyunsung <12345ggh81@gmail.com>
2021-11-09 21:13:58 +0900

    [CREATE] domain 와 presentation 를 이어주는 UseCase 추가

---
+++ domain/src/main/java/com/gsm/domain/usecase/profile/GetProfileUseCase.kt
@@ -0,0 +1,19 @@
+package com.gsm.domain.usecase.profile
+
+import com.gsm.domain.base.ParamsUseCase
+import com.gsm.domain.entity.request.GetProfileRequestEntity
+import com.gsm.domain.entity.response.GetProfileEntity
+import com.gsm.domain.repository.ProfileRepository
+import javax.inject.Inject
+
+class GetProfileUseCase @Inject constructor(private val repository: ProfileRepository) :
+    ParamsUseCase<GetProfileUseCase.Params,GetProfileEntity>() {
+
+    data class Params(
+        val request: GetProfileRequestEntity
+    )
+
+    override suspend fun buildUseCaseObservable(params: Params): GetProfileEntity {
+       return repository.getProfile(params.request)
+    }
+}
