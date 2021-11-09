3f65bc278b8173c6f6ac1d271d007d822aee05f1
ckrudals <2004ckck@naver.com>
2021-11-09 18:04:23 +0900

    [CREATE] DataStore로 token 저장하기

---
+++ presentation/src/main/java/com/gsm/presentation/util/Constant.kt
@@ -0,0 +1,10 @@
+package com.gsm.presentation.util
+
+class Constant {
+    companion object{
+        const val PREFERENCE_NAME="token_preference"
+        const val DEFAULT_TOKEN="default token"
+        const val PREFERENCES_TOKEN = "preferences_token"
+
+    }
+}
--- presentation/src/main/java/com/gsm/presentation/viewmodel/sign/in/SignInViewModel.kt
+++ presentation/src/main/java/com/gsm/presentation/viewmodel/sign/in/SignInViewModel.kt
@@ -8,8 +8,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.gsm.domain.entity.request.sign.TokenEntity
 import com.gsm.domain.usecase.LoginUseCase
+import com.gsm.presentation.DataStoreRepository
+import com.gsm.presentation.util.Constant.Companion.DEFAULT_TOKEN
 import com.gsm.presentation.util.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import java.lang.Exception
@@ -17,26 +20,39 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SignInViewModel @Inject constructor(
-    private val loginUseCase: LoginUseCase
+    private val loginUseCase: LoginUseCase,
+    private val dataStore: DataStoreRepository
 ) : ViewModel() {
 
 
-    private val _successMessage = MutableLiveData<Event<Boolean>>()
-    val successMessage: LiveData<Event<Boolean>> get() = _successMessage
+    var token = DEFAULT_TOKEN
+    val readToken = dataStore.readToken
+
+    private val _tokenValue = MutableLiveData<Event<String>>()
+    val tokenValue: LiveData<Event<String>> get() = _tokenValue
+
+    // 토큰을 저장한다.
+    fun saveToken(token: String) =
+        viewModelScope.launch(IO) {
+            Log.d("Token", "saveToken: $token")
+            dataStore.saveToken(token)
+        }
+
     suspend fun postLogin(token: String) = viewModelScope.launch {
 
         try {
             loginUseCase.buildUseCaseObservable(LoginUseCase.Params(TokenEntity(token))).let {
-                if (it.toString().isNotEmpty()) {
-                    _successMessage.value = Event(true)
+                Log.d("Token", "success ${it.success} message : ${it.message} token  ")
+                if (it.success == true) {
+                    _tokenValue.value = Event(it.token!!)
 
                 } else {
-                    _successMessage.value = Event(false)
+                    _tokenValue.value = Event("")
                 }
             }
-        } catch (e:Exception){
+        } catch (e: Exception) {
             Log.e("TAG", "postLogin: $e")
-            _successMessage.value = Event(false)
+            _tokenValue.value = Event("")
 
         }
     }
