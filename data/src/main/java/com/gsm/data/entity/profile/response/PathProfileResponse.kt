4e601c509bfd00960c1b0a7eb9f8e0449bc7cbd0
choiyunsung <12345ggh81@gmail.com>
2021-11-09 21:11:07 +0900

    [CREATE] data 로 값을 실어주기위한 domain 딴의 Entity 추가

---
+++ data/src/main/java/com/gsm/data/entity/profile/response/PathProfileResponse.kt
@@ -0,0 +1,26 @@
+package com.gsm.data.entity.profile.response
+
+import com.google.gson.annotations.SerializedName
+
+data class PathProfileResponse(
+    @SerializedName("id")
+    val id : String,
+    @SerializedName("sub")
+    val sub : String,
+    @SerializedName("name")
+    val name : String,
+    @SerializedName("profileImg")
+    val profileImg : String,
+    @SerializedName("isJobSeeker")
+    val isJobSeeker : Boolean,
+    @SerializedName("companyEmail")
+    val companyEmail : String,
+    @SerializedName("authCompany")
+    val authCompany : Boolean,
+    @SerializedName("purpose")
+    val purpose : String,
+    @SerializedName("level")
+    val level: Int,
+    @SerializedName("exp")
+    val exp : Int
+)
---
+++ domain/src/main/java/com/gsm/domain/entity/response/PathProfileEntity.kt
@@ -0,0 +1,26 @@
+package com.gsm.domain.entity.response
+
+import com.google.gson.annotations.SerializedName
+
+data class PathProfileEntity(
+    @SerializedName("id")
+    val id : String,
+    @SerializedName("sub")
+    val sub : String,
+    @SerializedName("name")
+    val name : String,
+    @SerializedName("profileImg")
+    val profileImg : String,
+    @SerializedName("isJobSeeker")
+    val isJobSeeker : Boolean,
+    @SerializedName("companyEmail")
+    val companyEmail : String,
+    @SerializedName("authCompany")
+    val authCompany : Boolean,
+    @SerializedName("purpose")
+    val purpose : String,
+    @SerializedName("level")
+    val level: Int,
+    @SerializedName("exp")
+    val exp : Int
+)
