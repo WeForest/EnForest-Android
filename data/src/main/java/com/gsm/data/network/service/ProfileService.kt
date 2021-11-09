5b313404c13b73d7ad98591c7897739ed48df45c
choiyunsung <12345ggh81@gmail.com>
2021-11-09 21:11:34 +0900

    [CREATE] Retrofit 통신을 위한 service 파일 추가

--- data/src/main/java/com/gsm/data/network/service/ProfileService.kt
+++ data/src/main/java/com/gsm/data/network/service/ProfileService.kt
@@ -1,12 +1,19 @@
 package com.gsm.data.network.service
-import com.gsm.data.entity.profile.response.GetNickNameResopnse
+import com.gsm.data.entity.profile.request.GetProfileRequest
+import com.gsm.data.entity.profile.response.GetProfileResponse
+import com.gsm.data.entity.profile.response.PathProfileResponse
 import retrofit2.http.*
 
 interface ProfileService {
 
     @GET("/profile/{nickname}")
     suspend fun viewProfile(
-        @Path("nickname") nickname: String
-    ): GetNickNameResopnse
+        @Path("nickname") request : GetProfileRequest
+    ): GetProfileResponse
+
+    @PATCH("/profile/update")
+    suspend fun pathProfile(
+        @Header("authorization") authorization: String
+    ) : PathProfileResponse
 
 }
