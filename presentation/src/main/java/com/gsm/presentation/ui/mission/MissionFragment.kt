be1e89418b1eee07ff02423be55727704293db48
ckrudals <2004ckck@naver.com>
2021-11-10 17:02:31 +0900

    [ADD] missionList adapter 연동

--- presentation/src/main/java/com/gsm/presentation/ui/mission/MissionFragment.kt
+++ presentation/src/main/java/com/gsm/presentation/ui/mission/MissionFragment.kt
@@ -7,17 +7,25 @@ import android.content.Intent
 import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
-import androidx.navigation.findNavController
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.chip.Chip
 import com.gsm.presentation.R
 import com.gsm.presentation.adapter.MissionAdapter
 import com.gsm.presentation.base.BaseFragment
 import com.gsm.presentation.databinding.FragmentMissionBinding
+import com.gsm.presentation.ui.mission.notification.MissionNotification
 import com.gsm.presentation.viewmodel.mission.MissionViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import java.util.*
+
 
 /*
 todo : 하루에 하나씩 미션이 울려 저장을 해야함 내부 db 사용 헤애힘 !!
  */
+@AndroidEntryPoint
 class MissionFragment : BaseFragment<FragmentMissionBinding>(R.layout.fragment_mission) {
     private val missionAdapter: MissionAdapter by lazy { MissionAdapter() }
     private val viewModel: MissionViewModel by viewModels<MissionViewModel>()
@@ -25,9 +33,19 @@ class MissionFragment : BaseFragment<FragmentMissionBinding>(R.layout.fragment_m
         binding.fragment = this@MissionFragment
     }
 
+    var chipText = "daily"
+
+    val time: Long = 1000 * 60 * 60
+    var number: Int = 1
+
     override fun FragmentMissionBinding.onViewCreated() {
+        getMissionType(DAY, 1)
+        chipClickType()
         setAdapter()
         alarmManager()
+        setTimer()
+        getMission()
+        observeGetMissionType()
     }
 
 
@@ -38,8 +56,7 @@ class MissionFragment : BaseFragment<FragmentMissionBinding>(R.layout.fragment_m
         intent.action = "MyBroadcastReceiverAction"
         val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0)
 
-        val msUntilTriggerHour: Long = 10000
-        val alarmTimeAtUTC: Long = System.currentTimeMillis() + msUntilTriggerHour
+        val alarmTimeAtUTC: Long = System.currentTimeMillis() + time
 
 
         if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.M) {
@@ -58,6 +75,51 @@ class MissionFragment : BaseFragment<FragmentMissionBinding>(R.layout.fragment_m
         }
     }
 
+
+    private fun setTimer() {
+        val timer = Timer()
+        val task: TimerTask = object : TimerTask() {
+            override fun run() {
+                Log.d("시간", "run: $time ")
+            }
+        }
+        timer.schedule(task, 0L, time)
+
+    }
+
+    private fun getMissionType(type: String, page: Int) {
+        lifecycleScope.launch {
+            viewModel.getMissionType(type, page)
+        }
+    }
+
+    // 각각의 type에 adapter를 refresh 해준다.
+    private fun chipClickType() {
+        binding.chipType.setOnCheckedChangeListener { group, selectedChipId ->
+            val chip = group.findViewById<Chip>(selectedChipId)
+            val selectedMealType = chip.text.toString().lowercase(Locale.ROOT)
+            chipText=selectedMealType
+            when (chipText) {
+                DAY -> getMissionType(DAY, 1)
+                WEEK -> getMissionType(WEEK, 1)
+                MONTH -> getMissionType(MONTH, 1)
+            }
+        }
+
+    }
+
+
+    private fun observeGetMissionType() {
+        viewModel.missionPageData.observe(viewLifecycleOwner) {
+            Log.d("mission", "observeGetMissionType: ${it} ")
+            when (it[0].type) {
+                DAY -> missionAdapter.setData(it)
+                WEEK -> missionAdapter.setData(it)
+                MONTH -> missionAdapter.setData(it)
+            }
+        }
+    }
+
     private fun setAdapter() {
         binding.recyclerView.apply {
 
@@ -68,7 +130,28 @@ class MissionFragment : BaseFragment<FragmentMissionBinding>(R.layout.fragment_m
         }
     }
 
+    private fun getMission() {
+        lifecycleScope.launch {
+            number.plus(1)
+            viewModel.getMission(number)
+
+            Log.d("number", "getMission:  number : ${number}")
+        }
+    }
+
     fun writeLayoutClick(view: View?) {
-        view?.findNavController()?.navigate(R.id.action_fragmentMission_to_bottomSheetMissionDialog)
+        // 현재 navigation 위치가 mssionFragment 일때만 화면이동한다.
+
+        if (findNavController().currentDestination?.id == R.id.missionFragment)
+            findNavController().navigate(
+                R.id.action_fragmentMission_to_bottomSheetMissionDialog
+            )
+
+    }
+
+    companion object {
+        const val DAY = "daily"
+        const val WEEK = "weekly"
+        const val MONTH = "monthly"
     }
 }
