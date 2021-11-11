9971aae7b94613cff24de190a18abfb5ca457a3a
ckrudals <2004ckck@naver.com>
2021-11-11 22:48:52 +0900

    [CREATE] missionRemoteDataSource

---
+++ data/src/main/java/com/gsm/data/datasource/mission/remote/MissionRemoteDataSource.kt
@@ -0,0 +1,13 @@
+package com.gsm.data.datasource.mission.remote
+
+import com.gsm.data.entity.mission.response.GetMissionResponse
+
+interface MissionRemoteDataSource {
+    suspend fun insertMission(mission: GetMissionResponse)
+    suspend fun deleteMission(mission: GetMissionResponse)
+
+
+    fun getMission(type:String): List<GetMissionResponse>
+
+
+}
