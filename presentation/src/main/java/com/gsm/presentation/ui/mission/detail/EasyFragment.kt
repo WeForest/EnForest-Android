d72b381fb432c284463c5f87941e9cffe30550df
ckrudals <2004ckck@naver.com>
2021-11-14 04:18:10 +0900

    [ADD] dataBase에 저장된 data recyclerView에 넣음

--- presentation/src/main/java/com/gsm/presentation/ui/mission/detail/EasyFragment.kt
+++ presentation/src/main/java/com/gsm/presentation/ui/mission/detail/EasyFragment.kt
@@ -1,11 +1,15 @@
 package com.gsm.presentation.ui.mission.detail
 
+import android.util.Log
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.gsm.domain.entity.mission.GetMissionEntity
 import com.gsm.domain.entity.mission.GetMissionTypePageEntity
 import com.gsm.presentation.R
 import com.gsm.presentation.adapter.MissionAdapter
+import com.gsm.presentation.adapter.MissionGetAdapter
 import com.gsm.presentation.base.BaseFragment
 import com.gsm.presentation.databinding.FragmentMissionEasyBinding
 import com.gsm.presentation.ui.mission.MissionDetailFragmentArgs
@@ -13,15 +17,16 @@ import com.gsm.presentation.ui.mission.MissionFragment.Companion.DAY
 import com.gsm.presentation.ui.mission.MissionFragment.Companion.MONTH
 import com.gsm.presentation.ui.mission.MissionFragment.Companion.WEEK
 import com.gsm.presentation.viewmodel.mission.MissionRemoteViewModel
+import com.gsm.presentation.viewmodel.mission.MissionViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class EasyFragment : BaseFragment<FragmentMissionEasyBinding>(R.layout.fragment_mission_easy) {
-    private val args = MissionDetailFragmentArgs()
-    private val remoteViewModel: MissionRemoteViewModel by viewModels()
 
-    private val missionAdapter: MissionAdapter by lazy { MissionAdapter() }
+    private val remoteViewModel: MissionRemoteViewModel by viewModels()
+    private val missionViewModel: MissionViewModel by activityViewModels()
+    private val missionAdapter: MissionGetAdapter by lazy { MissionGetAdapter() }
     override fun FragmentMissionEasyBinding.onCreateView() {
 
     }
@@ -33,17 +38,18 @@ class EasyFragment : BaseFragment<FragmentMissionEasyBinding>(R.layout.fragment_
     }
 
     private fun getMissionData() {
-        when (args.type) {
+        Log.d("TAG", "getMissionData:${missionViewModel.type.value} ")
+        when (missionViewModel.type.value) {
             DAY -> {
                 lifecycleScope.launch {
-                    remoteViewModel.getMission(DAY)
+                    remoteViewModel.getMission(DAY, LOW)
                 }
             }
             WEEK -> lifecycleScope.launch {
-                remoteViewModel.getMission(WEEK)
+                remoteViewModel.getMission(WEEK, LOW)
             }
             MONTH -> lifecycleScope.launch {
-                remoteViewModel.getMission(MONTH)
+                remoteViewModel.getMission(MONTH, LOW)
             }
 
         }
@@ -51,7 +57,9 @@ class EasyFragment : BaseFragment<FragmentMissionEasyBinding>(R.layout.fragment_
 
     private fun observeMission() {
         remoteViewModel.getMission.observe(viewLifecycleOwner) {
-            missionAdapter.setData(listOf(it as GetMissionTypePageEntity))
+            if (it != null) {
+                missionAdapter.setData(it)
+            }
         }
     }
 
@@ -63,6 +71,12 @@ class EasyFragment : BaseFragment<FragmentMissionEasyBinding>(R.layout.fragment_
                 LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
             setHasFixedSize(false)
         }
+    }
+
+    companion object {
+        const val LOW = "low"
+        const val MIDDLE = "middle"
+        const val HIGH = "high"
     }
 
 }
