30477f3849d6914360e19e5a0f8084e78051d8dd
choiyunsung <12345ggh81@gmail.com>
2021-11-15 20:12:54 +0900

    [CREATE] Login 로직

--- domain/src/main/java/com/gsm/domain/entity/request/GetProfileRequestEntity.kt
+++
@@ -1,8 +0,0 @@
-package com.gsm.domain.entity.request
-
-import com.google.gson.annotations.SerializedName
-
-data class GetProfileRequestEntity (
-    @SerializedName("nickname")
-    val nickname : String
-)
--- presentation/src/main/java/com/gsm/presentation/ui/profile/activity/MainProfileActivity.kt
+++ presentation/src/main/java/com/gsm/presentation/ui/profile/activity/MainProfileActivity.kt
@@ -3,10 +3,12 @@ package com.gsm.presentation.ui.profile.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gsm.presentation.R
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_profile)
     }
 }
