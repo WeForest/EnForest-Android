6d2d106dd1bcaae4cf76f8ea7c2cc7cd2de5eece
ckrudals <2004ckck@naver.com>
2021-11-16 20:36:33 +0900

    [UPDATE] baseFragment

--- presentation/src/main/java/com/gsm/presentation/base/BaseFragment.kt
+++ presentation/src/main/java/com/gsm/presentation/base/BaseFragment.kt
@@ -4,16 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 
-abstract class BaseFragment<B : ViewDataBinding>(
-    @LayoutRes val layoutId: Int
-) : Fragment() {
-    lateinit var binding: B
 open class BaseFragment<T : ViewDataBinding>(@LayoutRes val layoutRes: Int) : Fragment() {
 
     lateinit var binding: T
@@ -23,23 +18,6 @@ open class BaseFragment<T : ViewDataBinding>(@LayoutRes val layoutRes: Int) : Fr
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
-        init()
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.lifecycleOwner = this
-    }
-
-    abstract fun init()
-
-    protected fun shortShowToast(msg: String) =
-        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
-
-    protected fun longShowToast(msg: String) =
-        Toast.makeText(context, msg, Toast.LENGTH_LONG).show()
         binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
         binding.onCreateView()
         binding.onViewCreated()
