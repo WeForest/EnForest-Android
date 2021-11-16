828cfc36d3a797b3bea4541d960197e31f0ca1dc
ckrudals <2004ckck@naver.com>
2021-11-16 23:05:33 +0900

    [CREATE] study

---
+++ presentation/src/main/java/com/gsm/presentation/ui/study/StudyFragment.kt
@@ -0,0 +1,8 @@
+package com.gsm.presentation.ui.study
+
+import com.gsm.presentation.R
+import com.gsm.presentation.base.BaseFragment
+import com.gsm.presentation.databinding.FragmentStudyMeetingBinding
+
+class StudyFragment : BaseFragment<FragmentStudyMeetingBinding>(R.layout.fragment_study) {
+}
