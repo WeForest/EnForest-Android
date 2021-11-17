864a59fea0cdec1e37e3a3516d2aa091971768c3
ckrudals <2004ckck@naver.com>
2021-11-17 18:04:02 +0900

    [ADD] PREFERENCES Name

--- presentation/src/main/java/com/gsm/presentation/util/Constant.kt
+++ presentation/src/main/java/com/gsm/presentation/util/Constant.kt
@@ -2,18 +2,21 @@ package com.gsm.presentation.util
 
 class Constant {
     companion object{
-        const val PREFERENCE_NAME="token_preference"
-        const val DEFAULT_TOKEN="default token"
+        // 알림 시간 설정
+        const val ALARM_TIMER = 5
         const val DAY = 1
         const val MONTH = 30
         const val WEEK = 7
         const val NOTIFICATION_ID = 0
         const val MISSION_CHANNEL_ID = "notification_channel"
 
-        // 알림 시간 설정
-        const val ALARM_TIMER = 5
+
+        const val PREFERENCE_NAME="token_preference"
+        const val DEFAULT_TOKEN="default token"
+        const val DEFAULT_NAME="default name"
         const val DATABASE_NAME="mission"
         const val PREFERENCES_TOKEN = "preferences_token"
+        const val PREFERENCES_USER_NAME = "preferences_user_name"
 
     }
 }
