2581e09da98d777ce30047551acef5f98315f4d9
ckrudals <2004ckck@naver.com>
2021-11-23 16:00:48 +0900

    init

---
+++ presentation/src/main/java/com/gsm/presentation/ui/study/chat/ChatModel.kt
@@ -0,0 +1,6 @@
+package com.gsm.presentation.ui.study.chat
+
+import com.google.gson.annotations.SerializedName
+
+class ChatModel(val name: String, val script:String, val profile_image:String, val date_time:String){
+}
